726ca2392aadf908209ed4fca67bab0455e25fac
Edward Sparks <edd_sparks@hotmail.com>
2018-04-27 11:36:46 +0100

    changed the db

--- db/migrate/20180425195425_create_runner_prefs.rb
+++ db/migrate/20180425195425_create_runner_prefs.rb
@@ -1,7 +1,7 @@
 class CreateRunnerPrefs < ActiveRecord::Migration[5.1]
   def change
     create_table :runner_prefs do |t|
-      t.references :preferences, foreign_key: true
+      t.references :preference, foreign_key: true
       t.references :runner, foreign_key: true
 
       t.timestamps
